084d7af5c0a12b6e81b6199716f59a4355c47703
Scotty Logan <swl@stanford.edu>
2016-01-28 16:27:37 -0800

    added more profile debugging

--- config/passport.js
+++ config/passport.js
@@ -1,31 +1,40 @@
 var SamlStrategy = require('passport-saml').Strategy
+var oid2name = require('./oid2name');
 
 module.exports = function (passport, config) {
 
-	passport.serializeUser(function(user, done) {
-		done(null, user);
-	});
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
 
-	passport.deserializeUser(function(user, done) {
-		done(null, user);
-	});
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
 
-	passport.use(new SamlStrategy(
-	  {
-	    path: config.passport.saml.path,
-	    entryPoint: config.passport.saml.entryPoint,
-	    issuer: config.passport.saml.issuer
-	  },
-	  function(profile, done) {
-		return done(null,
-			{
-				id : profile.uid,
-				email : profile.email,
-				displayName : profile.cn,
-				firstName : profile.givenName,
-  				lastName : profile.sn
-			});
-	  })
-	);
+  passport.use(new SamlStrategy(
+    {
+      path: config.passport.saml.path,
+      entryPoint: config.passport.saml.entryPoint,
+      issuer: config.passport.saml.issuer
+    },
+    function(profile, done) {
+      var user = {};
+      console.error('PROFILE BEFORE');
+      console.dir(profile);
+      oid2name(profile);
+      console.error('PROFILE AFTER');
+      console.dir(profile);
+      user = {
+        id : profile.eduPersonPrincipalName,
+        email : profile.email,
+        displayName : profile.cn || profile.displayName,
+        firstName : profile.givenName,
+        lastName : profile.sn
+      };
+      console.error('USER');
+      console.dir(user);
+      return done(null, user);
+    }
+  ));
 
 }
